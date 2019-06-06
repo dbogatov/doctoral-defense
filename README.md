c1e3d321d9819e12f54f7d806caed0efd1b3676b
Dmytro Bogatov <dmytro@dbogatov.org>
2019-06-06 18:09:19 -0400

    Adapt PathORAM presentation.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Presentation title
+
+The up-to-date version of the document is built in CI and resides as artifact.
+
+> FIXME
+
+- [Download the file](https://git.dbogatov.org/templates/latex-prsentation/-/jobs/artifacts/master/raw/presentation.pdf?job=artifacts)
+- [View the file](https://git.dbogatov.org/templates/latex-prsentation/-/jobs/artifacts/master/file/presentation.pdf?job=artifacts)
+
+## How to compile
+
+```bash
+./document/build.sh # to compile without notes
+./document/build.sh -n # to compile wit notes
+open ./document/dist/*.pdf # to open
+```
---
+++ document/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -e
+shopt -s extglob 
+
+# Ensure that the CWD is set to script's location
+cd "${0%/*}"
+CWD=$(pwd)
+
+INTERACTION=nonstopmode
+OUTDIR=dist
+JOBNAME=presentation
+NOTES=""
+SUFFIX=""
+SYNCTEX=1
+
+usage() { echo "Usage: $0 [-o]" 1>&2; exit 1; }
+
+while getopts "n" o; do
+	case "${o}" in
+		n)
+			NOTES="\def\generatenotes{true}"
+			SUFFIX="-with-notes"
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+rm -rf ${OUTDIR}
+mkdir -p ${OUTDIR}
+
+if [ -n "$CI_BUILD_REF" ];
+then
+	printf "\providecommand{\\\version}{%s}" $(echo $CI_BUILD_REF | cut -c1-8) > version.tex
+fi
+
+latexmk -cd -f- -pdf -time \
+	-jobname=$JOBNAME$SUFFIX \
+	-outdir=$OUTDIR \
+	--synctex=${SYNCTEX} \
+	--interaction=${INTERACTION} \
+	-halt-on-error \
+	-pdflatex='xelatex %O "\def\dummy{} '${NOTES}' \input %S "' \
+	main
+
+rm -f ${OUTDIR}/*.{aux,log,out,xwm,toc,lof,lot,bib,bbl,bcf,blg,xml,fls,fdb_latexmk,nav,snm}
+
+echo "Done."
